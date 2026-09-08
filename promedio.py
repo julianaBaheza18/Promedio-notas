d2a0ff9e0ede3b3714bff2ca7f95acb71480383a
julianaBaheza18 <julianaceron603@gmail.com>
2026-09-07 20:54:19 -0500

    Add function to calculate average of two grades

---
+++ promedio.py
@@ -0,0 +1,13 @@
+def calcular_promedio(nota1, nota2):
+    promedio = (nota1 + nota2) / 2
+    return promedio
+
+
+if __name__ == "__main__":
+    nota1 = 8
+    nota2 = 9
+
+    resultado = calcular_promedio(nota1, nota2)
+
+    print("El promedio es:", resultado)
+  
